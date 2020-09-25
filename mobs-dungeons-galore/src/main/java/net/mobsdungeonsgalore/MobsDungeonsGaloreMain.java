accd72aa3b25d042bda9395dd194efe42e4278af
ThomasRFiedler <70819983+ThomasRFiedler@users.noreply.github.com>
2020-09-25 17:31:50 -0400

    Update MobsDungeonsGaloreMain.java
    added SoundInit

--- mobs-dungeons-galore/src/main/java/net/mobsdungeonsgalore/MobsDungeonsGaloreMain.java
+++ mobs-dungeons-galore/src/main/java/net/mobsdungeonsgalore/MobsDungeonsGaloreMain.java
@@ -6,10 +6,9 @@ import net.mobsdungeonsgalore.init.EntityInit;
 public class MobsDungeonsGaloreMain implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		
 		EntityInit.Init();
+		SoundInit.Init();
 		
 		System.out.println("Hello Fabric world!");
 	}
