d54b4e7c654f36f178a213f81a1e4f54583d4e6c
ThomasRFiedler <70819983+ThomasRFiedler@users.noreply.github.com>
2020-09-26 12:02:12 -0400

    Add files via upload

---
+++ main/java/net/mobsdungeonsgalore/MobsDungeonsGaloreMain.java
@@ -0,0 +1,16 @@
+package net.mobsdungeonsgalore;
+
+import net.fabricmc.api.ModInitializer;
+import net.mobsdungeonsgalore.init.EntityInit;
+import net.mobsdungeonsgalore.init.SoundInit;
+
+public class MobsDungeonsGaloreMain implements ModInitializer 
+{
+	@Override
+	public void onInitialize() 
+	{
+		EntityInit.Init();
+		SoundInit.Init();
+		
+	}
+}
